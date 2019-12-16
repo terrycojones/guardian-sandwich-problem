639aef5e91c50a57f21dd7779f03ae3951d6668f
terrycojones <terry@jon.es>
2019-12-16 18:14:53 +0100

    Reverse the order in which free indices are looked for to more quickly rule out possibilities. Code runs in 0.17 seconds now, down from 0.7 seconds.

--- solve-faster.py
+++ solve-faster.py
@@ -15,7 +15,7 @@ def _solve(availableDigitCounts, template, solutions):
     freeSlotCount = sum(slot is None for slot in template)
     if freeSlotCount:
         assert sum(availableDigitCounts) == freeSlotCount
-        for i in range(len(availableDigitCounts)):
+        for i in range(len(availableDigitCounts) - 1, -1, -1):
             if availableDigitCounts[i]:
                 firstFreeIndex = i
                 digit = firstFreeIndex + 1
@@ -51,14 +51,19 @@ solve([2] * 4)
 solve([3] * 9)
 
 
+# $ time solve-faster.py
+
 # solutions for [2, 2, 2, 2]
-# 23421314
 # 41312432
+# 23421314
 
 # solutions for [3, 3, 3, 3, 3, 3, 3, 3, 3]
-# 181915267285296475384639743
-# 191218246279458634753968357
 # 191618257269258476354938743
-# 347839453674852962752816191
+# 191218246279458634753968357
+# 181915267285296475384639743
 # 347936483574692582762519181
 # 753869357436854972642812191
+# 347839453674852962752816191
+
+# 0.17user 0.00system 0:00.17elapsed 98%CPU (0avgtext+0avgdata 8180maxresident)k
+# 0inputs+0outputs (0major+1121minor)pagefaults 0swaps
